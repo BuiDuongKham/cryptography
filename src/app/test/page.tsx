a162f701e93414f850bada246a8e0cd7ea90788a
Manchester-United-in-my-heart <129467586+BuiDuongKham@users.noreply.github.com>
2023-08-24 11:35:47 +0700

    siuuuu. Done UI

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -6,12 +6,6 @@ import {OneTimePad} from "../../../functions/onetimepad";
 import {Des} from "../../../functions/des";
 
 export default function Home() {
-	// @ts-ignore
-	const myDes = new Des('133457799BBCDFF1')
-	const plainText = '0123456789ABCDEF'
-	const cipherText = myDes.encrypt(plainText)
-	console.log(cipherText)
-	console.log(myDes.decrypt(cipherText))
 	return(
 		<></>
 	)
