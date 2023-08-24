4d461a8ae2ef3b3dc9b34a9710431f69266ab9ec
Manchester-United-in-my-heart <129467586+BuiDuongKham@users.noreply.github.com>
2023-08-24 11:21:42 +0700

    fucking siuuu. Done class

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -6,8 +6,12 @@ import {OneTimePad} from "../../../functions/onetimepad";
 import {Des} from "../../../functions/des";
 
 export default function Home() {
-	const myDes: Des = new Des('0000111000110010100100100011001011101010011011010000110101110011')
-	myDes.encrypt('Yourlipsaresmootherthanvaseline')
+	// @ts-ignore
+	const myDes = new Des('133457799BBCDFF1')
+	const plainText = '0123456789ABCDEF'
+	const cipherText = myDes.encrypt(plainText)
+	console.log(cipherText)
+	console.log(myDes.decrypt(cipherText))
 	return(
 		<></>
 	)
