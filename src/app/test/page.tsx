773b28868e571c1d20c09b336bebfd7deb56c794
Manchester-United-in-my-heart <129467586+BuiDuongKham@users.noreply.github.com>
2023-08-27 05:23:51 +0700

    init aes

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -4,6 +4,7 @@ import {Vigenere} from "../../../functions/vigenere";
 import {Vernam} from "../../../functions/vernam";
 import {OneTimePad} from "../../../functions/onetimepad";
 import {Des} from "../../../functions/des";
+import {Aes} from "../../../functions/aes";
 
 export default function Home() {
 	return(
