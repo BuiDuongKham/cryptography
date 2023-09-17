b076ab23cd5db953a3c83051f203475e83f1f219
Manchester-United-in-my-heart <129467586+BuiDuongKham@users.noreply.github.com>
2023-09-17 17:59:42 +0700

    done Diffie-Hellman

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -10,8 +10,7 @@ import {BigIntOperator} from "../../../functions/BigIntOperator";
 import {RSA} from "../../../functions/RSA";
 
 export default function Home() {
-	// console.log(RSA.encrypt(BigInt("73"), BigInt("151"), BigInt("11"), 'How are you', 2));
-	// console.log(RSA.decrypt(BigInt("73"), BigInt("151"), BigInt("11"), [BigInt('10260'), BigInt('9489'), BigInt('1782'), BigInt('727'), BigInt('10032'), BigInt('9278')], 2))
+	console.log(BigIntOperator.getPrimitiveRootOfPrimeNumber(BigInt(7)));
 	return (
 		<></>
 	)
